ad0230bb3514bdec21308b86a20952ac422b4cca
hamdielemin <124746143+hamdielemin@users.noreply.github.com>
2024-01-25 07:24:45 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from keras.preprocessing import image
 
 app = Flask(__name__)
